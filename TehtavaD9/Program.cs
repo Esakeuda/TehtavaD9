8fd0c7365e3258c7fa1ebc6664fdb681a66f3bfa
Esakeuda <127740104+Esakeuda@users.noreply.github.com>
2023-08-24 15:50:27 +0300

    Add project files.

---
+++ TehtavaD9/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace JakojäännösOhjelma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Syötä jaettava luku: ");
+                int jaettava = int.Parse(Console.ReadLine());
+
+                Console.Write("Syötä jakaja: ");
+                int jakaja = int.Parse(Console.ReadLine());
+
+                // Kutsutaan funktiota LaskeJakojäännös ja tallennetaan sen palauttama arvo
+                int jakojäännös = LaskeJakojäännös(jaettava, jakaja);
+
+                Console.WriteLine($"Jakojäännös: {jakojäännös}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Virheellinen syöte. Syötä kaksi kokonaislukua.");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Nollalla ei voi jakaa.");
+            }
+        }
+
+        // Funktio LaskeJakojäännös laskee jakojäännöksen ja palauttaa sen
+        static int LaskeJakojäännös(int jaettava, int jakaja)
+        {
+            return jaettava % jakaja;  // % on modulo-operaattori, joka palauttaa jakojäännöksen
+        }
+    }
+}
